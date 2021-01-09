8616f908818fb62c6ab48137b3c6811bf5b7c9e4
szarlata <e.a.szarlata@gmail.com>
2021-01-09 11:47:56 +0100

    js wrong constructor removed

--- src/app/components/_buttonhall.js
+++ src/app/components/_buttonhall.js
@@ -1,14 +1,13 @@
 class ButtonHall {
-  constructor() {
-    this.text = "Hall of fame";
-  }
   render() {
-    const hallText = this.text.toUpperCase();
+    const text = "Hall of fame";
+    const hallText = text.toUpperCase();
     const hallButton = `<button class="buttonHall">
                 <i class="buttonHall__icon">iconhere</i>
                 <span class="buttonHall__text">${hallText}</span>
             </button>`;
     document.getElementById("swquiz-app").innerHTML += hallButton;
   }
+}
 
-  export default ButtonHall;
+export default ButtonHall;
