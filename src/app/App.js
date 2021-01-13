9011c452f86fa85f9edfa059c44c9ac1fca5afc0
ZibiH <zbigniew.holak@gmail.com>
2021-01-13 15:48:26 +0100

    Create Mode Rules component with whole functionality and styling

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,12 @@
 import MenuOptions from './components/MenuOptions';
 import GameModeName from './components/GameModeName';
 import UsersRanking from './components/UsersRanking';
+import ModeRules from './components/ModeRules';
 
 export const App = ({ options }) => {
   new MenuOptions().render();
   new GameModeName();
+  new ModeRules().renderRules();
   new UsersRanking().render([
     { name: 'dad', score: '10/20' },
     { name: 'dad', score: '10/20' },
