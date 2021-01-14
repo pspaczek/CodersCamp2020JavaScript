861733c1cd8cf4cc2ba3d080bde24c05b699443a
ZibiH <zbigniew.holak@gmail.com>
2021-01-14 22:20:50 +0100

    Add basic styles and view for image card

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,13 @@
 import MenuOptions from './components/MenuOptions';
+import PeopleImg from './components/PeopleImg';
 import UsersRanking from './components/UsersRanking';
 
-
-export const App = ({options}) => {
-  new MenuOptions().render()
-  new UsersRanking().render([{name: 'dad', score: '10/20'}, {name: 'dad', score: '10/20'}, {name: 'dad', score: '10/20'}])
-}
-
-
+export const App = ({ options }) => {
+  new MenuOptions().render();
+  new PeopleImg().render();
+  new UsersRanking().render([
+    { name: 'dad', score: '10/20' },
+    { name: 'dad', score: '10/20' },
+    { name: 'dad', score: '10/20' },
+  ]);
+};
