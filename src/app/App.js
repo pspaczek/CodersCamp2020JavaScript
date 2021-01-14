d93d0f9e7bb55c902ab7d707425b5fb68d389ef2
szarlata <e.a.szarlata@gmail.com>
2021-01-14 12:00:33 +0100

    js: icon generator added, scss: styling for icon added

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,19 @@
-export const App = ({options}) => {
-}
+import MenuOptions from './components/MenuOptions';
+import GameModeName from './components/GameModeName';
+import UsersRanking from './components/UsersRanking';
+import ModeRules from './components/ModeRules';
+import Button from './components/Button '; 
+
+export const App = ({ options }) => {
+  new MenuOptions().render();
+  new GameModeName();
+  new ModeRules().renderRules();
+  new UsersRanking().render([
+    { name: 'dad', score: '10/20' },
+    { name: 'dad', score: '10/20' },
+    { name: 'dad', score: '10/20' },
+  ]);
+  new Button().render();
+};
 
 
--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,20 +1,30 @@
 import renderElement from '../tools/renderElement'
 
 class Button {
-    constructor (text, className) {
-      this.text = text;
-      this.className = className;
-    };
-    
-    render() {
-      const button = renderElement({
-        tagHTML: "button",
-        className: `${this.className}`,
-        innerText: `${this.text}`
-     });
-  
-      return button;
-    }
+  constructor(text, className) {
+    this.text = text;
+    this.className = className;
   };
 
-export default Button 
+  render() {
+    const button = renderElement({
+      tagHTML: "button",
+      className: `${this.className}`,
+      innerText: `${this.text}`
+    });
+
+    return button;
+  }
+
+  
+  addIcon(iconClass) {
+    const icon = renderElement({
+      tagHTML: "i",
+      className: `${iconClass}`,
+      parentElement: document.getElementsByTagName("button")[0]
+    });
+    return icon;
+  }
+};
+
+export default Button
