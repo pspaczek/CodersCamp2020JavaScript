cef9c7eb57d94e9e5871530338a4ba2bc1b93734
DaveTheWebDev <74704398+DaveTheWebDev@users.noreply.github.com>
2021-01-17 11:47:05 +0100

    14 questions answers (#49)
    * question generator init
    
    * added returnAnswersObject
    
    * Answers init
    
    * added mode to checkIfCorrect
    
    * added computerChoose
    
    * fixed problem with render
    
    Co-authored-by: Zibi <69586207+ZibiH@users.noreply.github.com>

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,18 @@
-
-  
 import GameModeName from './components/GameModeName';
 import UsersRanking from './components/UsersRanking';
 import QuestionGenerator from './components/QuestionGenerator';
 import ModeRules from './components/ModeRules';
 import Button from './components/Button'; 
+import QuestionGenerator from './components/QuestionGenerator';
+import Answers from './components/Answers';
 import PeopleImg from './components/PeopleImg';
 import MenuOptions from './components/MenuOptions';
 
 export const App = ({options}) => {
-  new QuestionGenerator().returnAnswersObject('people')
+  new MenuOptions().render()
+  const mode = document.querySelector('.options__mode--active').dataset.mode;
+  const answersPromise = new QuestionGenerator().returnAnswersObject(mode)
+  new Answers().render(answersPromise)
   new MenuOptions().render();
   new GameModeName();
   new ModeRules().renderRules();
@@ -20,4 +23,3 @@ export const App = ({options}) => {
     { name: 'dad', score: '10/20' },
     { name: 'dad', score: '10/20' },
   ]);
-};
